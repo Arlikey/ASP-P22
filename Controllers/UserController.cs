e6f1a1b6a9bc27d1ec3deb9e1abf8dd931406846
admin <admin@DESKTOP-FA5KDQG>
2025-01-21 15:08:22 +0200

    completed validation of user registration

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using ASP_P22.Models.User;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace ASP_P22.Controllers
 {
@@ -10,9 +11,14 @@ namespace ASP_P22.Controllers
         {
             if (HttpContext.Session.Keys.Contains("formModel"))
             {
-                ViewData["formModel"] = JsonSerializer.Deserialize<UserSignUpFormModel>(
+                var userSignUpFormModel  = JsonSerializer.Deserialize<UserSignUpFormModel>(
                     HttpContext.Session.GetString("formModel")!
                 );
+                var (validationStatus, errors) = ValidateUserSignUpModel(userSignUpFormModel);
+                ViewData["formModel"]=userSignUpFormModel;
+                ViewData["validationStatus"] = validationStatus;
+                ViewData["errors"] = errors;
+
                 HttpContext.Session.Remove("formModel");
             }
             return View();
@@ -22,6 +28,82 @@ namespace ASP_P22.Controllers
             HttpContext.Session.SetString("formModel",
                 JsonSerializer.Serialize(userSignUpFormModel));
             return RedirectToAction("Index");
+        }
+        private (bool, Dictionary<string, string>) ValidateUserSignUpModel(UserSignUpFormModel? userSignUpFormModel) 
+        {
+            bool status = true;
+            Dictionary<string, string> errors = [];
+
+            if(userSignUpFormModel == null)
+            {
+                status = false;
+                errors["ModelState"] = "Модель не передано.";
+                return (status, errors);
+            }
+            if (string.IsNullOrEmpty(userSignUpFormModel.UserName))
+            {
+                status = false;
+                errors["UserName"] = "Ім'я не може бути порожнім.";
+            }
+            else if(!Regex.IsMatch(userSignUpFormModel.UserName, "^[A-ZА-Я].*"))
+            {
+                status = false;
+                errors["UserName"] = "Ім'я має починатися з великої літери.";
+            }
+
+
+            if (string.IsNullOrEmpty(userSignUpFormModel.UserEmail))
+            {
+                status = false;
+                errors["UserEmail"] = "Email не може бути порожнім.";
+            }
+            else if (!Regex.IsMatch(userSignUpFormModel.UserEmail, @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"))
+            {
+                status = false;
+                errors["UserEmail"] = "Email не відповідає шаблону.";
+            }
+
+
+            if (string.IsNullOrEmpty(userSignUpFormModel.UserLogin))
+            {
+                status = false;
+                errors["UserLogin"] = "Логін не може бути порожнім.";
+            }
+            else if (userSignUpFormModel.UserLogin.Contains(':'))
+            {
+                status = false;
+                errors["UserLogin"] = "Логін не повинен містити символ ':'.";
+            }
+
+
+            if (string.IsNullOrEmpty(userSignUpFormModel.Password1))
+            {
+                status = false;
+                errors["Password1"] = "Пароль не може бути порожнім.";
+            }
+            else if (userSignUpFormModel.Password1.Length < 8 || userSignUpFormModel.Password1.Length > 16)
+            {
+                status = false;
+                errors["Password1"] = "Пароль повинен містити від 8 до 16 символів.";
+            } 
+            else if (!Regex.IsMatch(userSignUpFormModel.Password1, @"^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*]).*$"))
+            {
+                status = false;
+                errors["Password1"] = "Пароль повинен містити принаймні одну літеру, одну цифру та один спеціальний символ (!@#$%^&*).";
+            }
+
+
+            if (string.IsNullOrEmpty(userSignUpFormModel.Password2))
+            {
+                status = false;
+                errors["Password2"] = "Пароль не може бути порожнім.";
+            }
+            else if (string.Compare(userSignUpFormModel.Password1, userSignUpFormModel.Password2) != 0)
+            {
+                status = false;
+                errors["Password2"] = "Паролі не співпадають.";
+            }
+            return (status, errors);
         }
         public IActionResult Review() 
         {
